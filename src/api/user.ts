8ea8b5a6f757f91d0247fdd4cf4161ba504b0af8
Gustavo Gomes <gustph.crg@gmail.com>
2025-05-20 21:36:15 +0100

    feat(login): wip login

---
+++ src/api/user.ts
@@ -0,0 +1,7 @@
+import axiosInstance from "./index"
+
+export async function getUsers(){
+    const{ data } = await axiosInstance.get('/users')
+
+    return data
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
+import { getUsers } from "./api/user";
 import { isLogged } from "./modules/login";
 
 const signButton = document.getElementById('signInButton') as HTMLButtonElement
 
 isLogged(false)
-if (signButton) {
-    signButton.addEventListener('click', (event) => {
-      event.preventDefault(); // Impede o comportamento padrão do formulário
-      isLogged(true);
-    });
-  }
+const users = await getUsers()
+console.log(users)
